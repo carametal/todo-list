6ff5daf8cb9740ac1685af10583a3deb6373a2f8
carametal <mkatsdevelop@gmail.com>
2023-12-09 17:44:01 +0900

    tasksテーブルをinit.sqlに追加

--- docker/postgres/init/01_init.sql
+++ docker/postgres/init/01_init.sql
@@ -1,16 +1,21 @@
-
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
-
 create table users(
-	id serial PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	username VARCHAR(50) COLLATE "C" UNIQUE NOT NULL,
 	password VARCHAR(500) NOT NULL,
 	enabled boolean NOT NULL
 );
 CREATE TABLE authorities (
-	id serial PRIMARY KEY,
-	user_id integer NOT NULL,
+	id SERIAL PRIMARY KEY,
+	user_id INTEGER NOT NULL,
 	authority VARCHAR(50) COLLATE "C" NOT NULL,
 	CONSTRAINT fk_authorities_users FOREIGN KEY(user_id) REFERENCES users(id)
 );
 create unique index ix_auth_username on authorities (user_id, authority);
+CREATE TABLE tasks (
+	task_id SERIAL PRIMARY KEY,
+	name VARCHAR(255) NOT NULL,
+	detail TEXT,
+	state VARCHAR(50) CHECK (state IN ('Todo', 'InProgress', 'Done')),
+	user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
