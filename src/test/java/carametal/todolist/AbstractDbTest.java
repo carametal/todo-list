c88a0e583323b54061f753ad4776854b0ff9ed43
carametal <mkatsdevelop@gmail.com>
2023-12-14 21:18:51 +0900

    testcontainersのwarningの解消

--- src/test/java/carametal/todolist/AbstractDbTest.java
+++ src/test/java/carametal/todolist/AbstractDbTest.java
@@ -9,22 +9,21 @@ import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @Testcontainers
 @SpringBootTest
 public class AbstractDbTest {
   @Container
-  private static PostgreSQLContainer postgres =
-      new PostgreSQLContainer()
-          .withDatabaseName("todo_list")
-          .withUsername("postgres")
-          .withPassword("postgres");
+  private static PostgreSQLContainer<?> postgres =
+      new PostgreSQLContainer<>(DockerImageName.parse(PostgreSQLContainer.IMAGE))
+          .withDatabaseName("todo_list");
 
   @DynamicPropertySource
   static void setup(DynamicPropertyRegistry registry) {
     registry.add("spring.datasource.url", postgres::getJdbcUrl);
-    // registry.add("spring.datasource.username", postgres::getUsername);
-    // registry.add("spring.datasource.password", postgres::getPassword);
+    registry.add("spring.datasource.username", postgres::getUsername);
+    registry.add("spring.datasource.password", postgres::getPassword);
   }
 
   @Test
